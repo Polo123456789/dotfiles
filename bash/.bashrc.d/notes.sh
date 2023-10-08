28ad7322de78dbe10e36d79de5036d09d260d79a
Polo123456789 <pabsan02@gmail.com>
2023-10-08 08:56:26 -0600

    (+)<bash> Add fzf to notes script

--- bash/.bashrc.d/notes.sh
+++ bash/.bashrc.d/notes.sh
@@ -3,12 +3,22 @@
 notes_dir="$HOME/docs/.notes/"
 
 n() {
-    local note_path="$notes_dir/$1"
-    local note_dir=$(dirname "$note_path")
     local current_dir=$(pwd)
+    local note_path=""
+    local note_dir=""
+    if [ -z "$1" ]; then
+        # Use fzf to open the notes
+        cd "$notes_dir"
+        note_path=$(fzf --reverse)
+        note_dir=$(dirname "$note_path")
+        note_path=$(basename "$note_path")
+    else
+        note_path="$notes_dir/$1"
+        note_dir=$(dirname "$note_path")
 
-    if [ ! -d "$note_dir" ]; then
-        mkdir -p "$note_dir"
+        if [ ! -d "$note_dir" ]; then
+            mkdir -p "$note_dir"
+        fi
     fi
 
     cd "$note_dir"
