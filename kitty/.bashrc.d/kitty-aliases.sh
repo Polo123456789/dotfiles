c7e083374fb312ae826f55265d3a3aa4f0983748
Polo123456789 <pabsan02@gmail.com>
2023-10-17 16:53:51 -0600

    (+)<kitty> Kitten integrations

--- kitty/.bashrc.d/kitty-aliases.sh
+++ kitty/.bashrc.d/kitty-aliases.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+alias kitten="kitty +kitten"
 alias ssh="kitty +kitten ssh"
+alias icat="kitty +kitten icat"
+alias diff="kitty +kitten diff"
+alias hg="kitty +kitten hyperlinked_grep"
