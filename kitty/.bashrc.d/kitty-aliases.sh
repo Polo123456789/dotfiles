3e74fcca783821b9e3efa9b1a06f43dc5cd06890
Polo123456789 <pabsan02@gmail.com>
2024-04-04 11:46:25 -0600

    (-)<kitty> Fix ssh in zellij

--- kitty/.bashrc.d/kitty-aliases.sh
+++ kitty/.bashrc.d/kitty-aliases.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 alias kitten="kitty +kitten"
-alias ssh="kitty +kitten ssh"
 alias icat="kitty +kitten icat"
 alias diff="kitty +kitten diff"
 alias hg="kitty +kitten hyperlinked_grep"
+
+# alias ssh="kitty +kitten ssh"
+ssh() {
+    if [ "$TERM" = "xterm-kitty" ] && [ -z "$ZELLIJ" ] ; then
+        kitty +kitten ssh "$@"
+    else
+        /usr/bin/ssh "$@"
+    fi
+}
