3554fc2b17436dd896d9059981054c0af8ca57b7
Polo123456789 <pabsan02@gmail.com>
2024-04-15 12:04:58 -0600

    (+)<bash>Discord notification for long commands

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -12,9 +12,6 @@ alias ..="cd .."
 alias today="date -Idate"
 alias today-entry="nvim $(date -Idate).md"
 
-
-alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
-
 alias persist-tmp="mv ~/tmp/* ~/.persist-tmp/"
 alias restore-tmp="mv ~/.persist-tmp/* ~/tmp/"
 
@@ -24,3 +21,12 @@ alias select-date="zenity --calendar --date-format='%Y-%m-%d'"
 
 # Last command first argument
 alias lfa='history | tail -n 2 | head -n 1 | awk '\''{print $3}'\'''
+
+
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+alert-discord() {
+    WEBHOOK=$(cat $HOME/.config/discord-alert-on-end/webhook)
+    last_command=$(history | tail -n 1 | sed -e 's/^\s*[0-9]\+\s*//;s/[;&|]\s*alert-discord$//')
+    curl -X POST -H "Content-Type: application/json" -d "{\"content\": \"Command \`$last_command\` has finished.\"}" $WEBHOOK
+}
