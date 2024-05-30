21fe0e9b51fdcd17c6f89f40ba40e228e68fb848
Polo123456789 <pabsan02@gmail.com>
2024-05-30 12:11:41 -0600

    (+)<bash> Cleanup of unused functions

--- bash/.bashrc.d/utils.sh
+++ bash/.bashrc.d/utils.sh
@@ -1,18 +1,12 @@
-back() {
-    cd "$OLDPWD"
-}
-
 from-history() {
     local cmd="$(history | awk '{$1=""; print $0}' | sort | uniq |fzf)"
-    echo $cmd
-    history -s "$cmd"
-    eval $cmd
+    echo "$cmd"
+	echo "$cmd" >> ~/.bash_history
+    eval "$cmd"
 }
 
 fzff () {
-    fzf -i < $1
-}
-
-notify-at() {
-    echo notify-send -u critical "$2" | at "$1"
+	local file=$1
+	shift
+	fzf -i $@ < $file
 }
