178e8ed463baf70186f6e6c6b53582461c5f978f
Polo123456789 <pabsan02@gmail.com>
2024-06-03 14:27:39 -0600

    (+) Use bash builtins

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -2,7 +2,8 @@ alias search-process="ps -ax | grep"
 alias exi="exit"
 alias fc="fc -e nvim"
 alias vim="nvim"
-alias ll='ls -alF'
+alias ls="ls --color=auto --hyperlink"
+alias ll='ls -alFh'
 alias la='ls -A'
 alias l='ls -CF'
 alias start="kitty"
@@ -20,7 +21,8 @@ alias rds="rclone-drive-sync"
 alias select-date="zenity --calendar --date-format='%Y-%m-%d'"
 
 # Last command first argument
-alias lfa='history | tail -n 2 | head -n 1 | awk '\''{print $3}'\'''
+# alias lfa='history | tail -n 2 | head -n 1 | awk '\''{print $3}'\'''
+alias lfa='echo "--hope-this-is-invalid-flag Just use Alt+."'
 
 
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
--- bash/.bashrc.d/utils.sh
+++ bash/.bashrc.d/utils.sh
@@ -1,8 +1,9 @@
 from-history() {
-    local cmd="$(history | awk '{$1=""; print $0}' | sort | uniq |fzf)"
-    echo "$cmd"
-	echo "$cmd" >> ~/.bash_history
-    eval "$cmd"
+    # local cmd="$(history | awk '{$1=""; print $0}' | sort | uniq |fzf)"
+    # echo "$cmd"
+	# echo "$cmd" >> ~/.bash_history
+    # eval "$cmd"
+	echo "Use Ctrl-r"
 }
 
 fzff () {
